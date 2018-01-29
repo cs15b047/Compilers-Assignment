8b0d154c4a966a12e759abed74084003b55844b4
Ajay Shridhar Joshi <cs15b047@smail.iitm.ac.in>
2018-01-29 15:19:24 +0530

    Add files via upload

---
+++ P4/P4.java
@@ -0,0 +1,17 @@
+import syntaxtree.*;
+import visitor.*;
+
+public class P4 {
+   public static void main(String [] args) {
+      try {
+         Node root = new MiniIRParser(System.in).Goal();
+         root.accept(new GJNoArguDepthFirst<String>()); // Your assignment part is invoked here.
+      }
+      catch (ParseException e) {
+         System.out.println(e.toString());
+      }
+   }
+} 
+
+
+
